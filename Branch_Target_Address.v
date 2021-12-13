f2dcdef37956002293eaf7c736fb03f0b539be8a
ChenZihang <919106840316@njust.edu.cn>
2021-12-13 09:48:44 +0800

    2021.12.13 Committed By ChenZihang

---
+++ Branch_Target_Address.v
@@ -0,0 +1,28 @@
+module Branch_Target_Address(
+    input clk,
+    input resetn,
+    input [31:0] pc
+);
+
+    wire [9:0] BTB_index;
+    wire [31:0] BTA;
+
+    reg [44:0] BTB1 [0:127];
+    reg [44:0] BTB2 [0:127];
+
+    assign BTB_index = {pc[25:21] ^ pc[20:16], pc[15:11] ^ pc[10:6]};
+    assign BTA = BTB1[pc[5:0]][44] == 1 ? (BTB1[pc[5:0]][43:34] == BTB_index ? BTB1[pc[5:0]][33:2]) : (BTB2[44] == 1 ? BTB2[pc[5:0]][43:34] == BTB_index ? BTB2[pc[5:0]][33:2] : (32'd0));
+
+/////////
+    always @(posedge clk) begin
+        if(~resetn) begin : initialize
+            integer i;
+            for(i = 0;i <= 127;i = i + 1) begin : loop
+                BTB1[i] = 45'd0;
+                BTB2[i] = 45'd0; 
+            end
+        end else begin
+            //BTB replace algorithm
+        end
+    end
+endmodule
--- branch_history.v
+++ branch_history.v
@@ -1,3 +1,41 @@
+module PHT_FSM(
+    input [1:0] old_state,
+    input branch_en,
+    input clk,
+    input resetn,
+    output reg [1:0] updated_state
+);
+    parameter Strongly_taken = 2'b11;
+    parameter Weakly_taken = 2'b10;
+    parameter Weakly_not_taken = 2'b01;
+    parameter Strongly_not_taken = 2'b00;
+
+    always @(*) begin
+        if(!resetn) begin
+            updated_state = Strongly_taken;
+        end else begin
+            case(updated_state)
+                Strongly_taken : begin
+                    updated_state = branch_en ? Strongly_taken : Weakly_taken;
+                end
+                Weakly_taken : begin
+                    updated_state = branch_en ? Strongly_taken : Weakly_not_taken;
+                end
+                Weakly_not_taken : begin
+                    updated_state = branch_en ? Weakly_taken : Strongly_not_taken;
+                end
+                Strongly_not_taken : begin
+                    updated_state = branch_en ? Weakly_not_taken : Strongly_not_taken;
+                end
+                default : begin
+                    
+                end
+            endcase
+        end
+    end
+    
+endmodule
+
 module branch_history(
     input [31:0] pc,
     input clk,
@@ -43,7 +81,7 @@ module branch_history(
                 BHT[i] <= 4'b0000;
             end
         end else begin
-            BHT[update_BHT_index] <= BHT[update_BHT_index] << 1 + {3'b000, branch_en};
+            BHT[update_BHT_index] <= BHT[update_BHT_index] << 1 + {3'b000, taken_en};
         end
     end
 
