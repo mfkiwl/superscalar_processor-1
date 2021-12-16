bd30826aeb61139952c6a54d07f4e4019bffe257
Kev1n <c1048271926@outlook.com>
2021-12-16 17:57:36 +0800

    solve the problem about cache line
    2021.12.16 17:57 Committed By ChenZihang

--- I_cache.v
+++ I_cache.v
@@ -1,44 +1,126 @@
-module I_cache(
-    input clk,
-    input resetn,
+module cache_fetch(
+    input wire [4:0] offset,
+    input wire [255:0] cache_data,
+    input wire [127:0] memory_data,
+    input wire hit,
+    output reg [32:0] inst0,
+    output reg [32:0] inst1,
+    output reg [32:0] inst2,
+    output reg [32:0] inst3
+);
 
-    input lookup_request,
-    input [31:0] pc,
-    output [127:0] fetch_pack,
+    always @(*) begin
+        if(hit) begin
+            case(offset)
+                5'b00000 : begin
+                    inst0 <= {1'b1, cache_data[255:224]};
+                    inst1 <= {1'b1, cache_data[223:192]};
+                    inst2 <= {1'b1, cache_data[191:160]};
+                    inst3 <= {1'b1, cache_data[159:128]};
+                end
+                5'b00100 : begin
+                    inst0 <= {1'b1, cache_data[223:192]};
+                    inst1 <= {1'b1, cache_data[191:160]};
+                    inst2 <= {1'b1, cache_data[159:128]};
+                    inst3 <= {1'b1, cache_data[127:96]};
+                end
+                5'b01000 : begin
+                    inst0 <= {1'b1, cache_data[191:160]};
+                    inst1 <= {1'b1, cache_data[159:128]};
+                    inst2 <= {1'b1, cache_data[127:96]};
+                    inst3 <= {1'b1, cache_data[95:64]};
+                end
+                5'b01100 : begin
+                    inst0 <= {1'b1, cache_data[159:128]};
+                    inst1 <= {1'b1, cache_data[127:96]};
+                    inst2 <= {1'b1, cache_data[95:64]};
+                    inst3 <= {1'b1, cache_data[63:32]};
+                end
+                5'b10000 : begin
+                    inst0 <= {1'b1, cache_data[127:96]};
+                    inst1 <= {1'b1, cache_data[95:64]};
+                    inst2 <= {1'b1, cache_data[63:32]};
+                    inst3 <= {1'b1, cache_data[31:0]};
+                end
+                5'b10100 : begin
+                    inst0 <= {1'b1, cache_data[95:64]};
+                    inst1 <= {1'b1, cache_data[63:32]};
+                    inst2 <= {1'b1, cache_data[31:0]};
+                    inst3 <= 33'd0;
+                end
+                5'b11000 : begin
+                    inst0 <= {1'b1, cache_data[63:32]};
+                    inst1 <= {1'b1, cache_data[31:0]};
+                    inst2 <= 33'd0;
+                    inst3 <= 33'd0;
+                end
+                5'b11000 : begin
+                    inst0 <= {1'b1, cache_data[31:0]};
+                    inst1 <= 33'd0;
+                    inst2 <= 33'd0;
+                    inst3 <= 33'd0;
+                end
+                default : begin
+                    inst0 <= 33'd0;
+                    inst1 <= 33'd0;
+                    inst2 <= 33'd0;
+                    inst3 <= 33'd0;
+                end
+            endcase
+        end else begin
+            inst0 <= {1'b1, memory_data[127:96]};
+            inst1 <= {1'b1, memory_data[95:64]};
+            inst2 <= {1'b1, memory_data[63:32]};
+            inst3 <= {1'b1, memory_data[31:0]};
+        end
+    end
+
+endmodule
+
+module I_cache(
+    input wire clk,
+    input wire resetn,
+    input wire lookup_request,
+    input wire [31:0] pc,
+    output wire [32:0] inst0,
+    output wire [32:0] inst1,
+    output wire [32:0] inst2,
+    output wire [32:0] inst3,
     //AXI4 interface
-    input [31:0] RDATA,
-    input ARREADY,
-    input RVALID,
-    output [31:0] ARADDR,
-    output ARVALID,
-    output RREADY
+    input wire [127:0] RDATA,
+    input wire ARREADY,
+    input wire RVALID,
+    output wire [31:0] ARADDR,
+    output wire ARVALID,
+    output wire RREADY
 );
 
     wire way0_hit;
     wire way1_hit;
 
-    wire [20:0] tagv0;
-    wire [20:0] tagv1;
+    wire [19:0] tagv0;
+    wire [19:0] tagv1;
 
     wire [7:0] index;
-    wire [3:0] offset;
-    wire [19:0] tag;
+    wire [18:0] tag;
+    wire [4:0] offset;
 
     wire [255:0] way0_data;
     wire [255:0] way1_data;
+
+    wire [255:0] cache_data;
+    wire hit;
     
-    wire [127:0] way0_fetch_pack;
-    wire [127:0] way1_fetch_pack;
 //////////
-    assign way0_hit = tagv0[20] & (tagv0[19:0] == tag);
-    assign way1_hit = tagv1[20] & (tagv1[19:0] == tag);
+    assign way0_hit = tagv0[19] & (tagv0[18:0] == tag);
+    assign way1_hit = tagv1[19] & (tagv1[18:0] == tag);
 
     assign index = pc[12:5];
-    assign offset = pc[3:0];
-    assign tag = pc[31:12]; //the length of tag should be modified
+    assign tag = pc[31:13];
+    assign offset = pc[4:0];
 
-    assign way0_fetch_pack = pc[4:2] == 3'b000 ? way0_data[255:128] : (pc[4:2] == 3'b001 ? way0_data[223:96] : (pc[4:2] == 3'b010 ? way0_data[191:64] : (pc[4:2] == 3'b011 ? way0_data[159:32] : way0_data[127:0])));
-    assign way1_fetch_pack = pc[4:2] == 3'b000 ? way1_data[255:128] : (pc[4:2] == 3'b001 ? way1_data[223:96] : (pc[4:2] == 3'b010 ? way1_data[191:64] : (pc[4:2] == 3'b011 ? way0_data[159:32] : way1_data[127:0])));
+    assign cache_data = way0_hit ? way0_data : way1_data;
+    assign hit = way0_hit | way1_hit;
 //////////
     parameter IDEL = 2'b00;
     parameter LOOKUP = 2'b01;
@@ -99,6 +181,17 @@ module I_cache(
         .addra(index),  // input wire [7 : 0] addra
         .dina(),    // input wire [20 : 0] dina
         .douta(tagv1)  // output wire [20 : 0] douta
+    );
+
+    cache_fetch cf(
+        .offset(offset),
+        .cache_data(cache_data),
+        .memory_data(RDATA),
+        .hit(hit),
+        .inst0(inst0),
+        .inst1(inst1),
+        .inst2(inst2),
+        .inst3(inst3)
     );
 
 endmodule
